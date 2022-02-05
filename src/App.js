f01e6edd9198c99f41bc1e9c0532fd930bafb517
rafikamirkarimova <tekhronkhudaykulov@gmail.com>
2022-02-05 11:55:49 +0500

    sadad

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './style/style.scss';
+import Main from "./main";
+
+const  App = () => {
+  return (
+    <div>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
